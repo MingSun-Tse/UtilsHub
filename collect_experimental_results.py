d929a11540409bdf84b55b049a78a11b068ab8aa
Huan Wang <huanw@zju.edu.cn>
2020-08-01 11:05:50 -0400

    improve collect_experimental_results.py

--- collect_experimental_results.py
+++ collect_experimental_results.py
@@ -14,13 +14,16 @@ def _get_value(line, key, type_func=float):
     value = type_func(line_seg[i + 1]) # example: "Acc: 0.7"
     return value
 
-def _get_exp_id(exp_path):
-    '''arg example: kd-vgg13vgg8-cifar100-Temp40_SERVER5-20200727-220318
+def _get_exp_name_id(exp_path):
+    '''arg example: Experiments/kd-vgg13vgg8-cifar100-Temp40_SERVER5-20200727-220318
+            or kd-vgg13vgg8-cifar100-Temp40_SERVER5-20200727-220318
     '''
+    exp_path = exp_path.strip('/')
     assert 'SERVER' in exp_path # safety check
     exp_id = exp_path.split('-')[-1]
     assert exp_id.isdigit() # safety check
-    return exp_id
+    exp_name = os.path.split(exp_path)[-1].split('_SERVER')[0]
+    return exp_name, exp_id
 
 def _get_project_name():
     cwd = os.getcwd()
@@ -37,34 +40,57 @@ def _make_acc_str(acc_list, num_digit=2):
     output = ', '.join(acc_str) + ' -- %s (%s)' % (mean, std)
     return output
 
-'''Usage:
-    In the project dir, run:
-    python ../UtilsHub/collect_experimental_results.py vid-wrn402wrn162-cifar100-HEVGGAugment_
-'''
-exp_kw = sys.argv[1]
-exp_id = []
-acc_last = []
-acc_best = []
+def print_acc_for_one_exp(all_exps, name):
+    '''In <all_exps>, pick those with <name> in their name for accuracy collection.
+    '''
+    exp_id = []
+    acc_last = []
+    acc_best = []
 
-exps = ['Experiments/%s' % x for x in os.listdir('Experiments') if exp_kw in x]
-exps.sort()
-for exp in exps:
-    log_f = '%s/log/log.txt' % exp
-    for line in open(log_f, 'r'):
-        if 'Epoch 240 (after update)' in line: # parsing accuracy
-            acc_l = _get_value(line, 'Acc1')
-            acc_b = _get_value(line, 'Best_Acc1')
-            break
-    acc_last.append(acc_l)
-    acc_best.append(acc_b)
-    exp_id.append(_get_exp_id(exp))
+    for exp in all_exps:
+        if name in exp:
+            log_f = '%s/log/log.txt' % exp
+            for line in open(log_f, 'r'):
+                if 'Epoch 240 (after update)' in line: # parsing accuracy
+                    acc_l = _get_value(line, 'Acc1')
+                    acc_b = _get_value(line, 'Best_Acc1')
+                    break
+            acc_last.append(acc_l)
+            acc_best.append(acc_b)
+            _, id = _get_exp_name_id(exp)
+            exp_id.append(id)
 
-# example for the exp_str and acc_last_str
-# 138-CRD-174550, 174554, 174558:
-# 75.84, 75.63, 75.45 – 75.64 (0.16)
-exp_str = '%s-%s-' % (os.environ['SERVER'], _get_project_name()) + ', '.join(exp_id) + ':'
-acc_last_str = _make_acc_str(acc_last)
-acc_best_str = _make_acc_str(acc_best)
-print(exp_str)
-print(acc_last_str)
-print(acc_best_str)
+    # example for the exp_str and acc_last_str:
+    # 174550, 174554, 174558 (138-CRD)
+    # 75.84, 75.63, 75.45 – 75.64 (0.16)
+    exp_str = ', '.join(exp_id) + ' (%s-%s)' % (os.environ['SERVER'], _get_project_name())
+    acc_last_str = _make_acc_str(acc_last)
+    acc_best_str = _make_acc_str(acc_best)
+    print(exp_str)
+    print(acc_last_str)
+    print(acc_best_str)
+
+def main():
+    '''Usage:
+        In the project dir, run:
+        python ../UtilsHub/collect_experimental_results.py 20200731-18
+    '''
+    kw = sys.argv[1]
+    all_exps = glob.glob('Experiments/*%s*' % kw)
+    all_exps.sort()
+    
+    # get independent exps, because each independent exp is run for multiple times.
+    independent_exps = []
+    for exp in all_exps:
+        name, _ = _get_exp_name_id(exp)
+        if name not in independent_exps:
+            independent_exps.append(name)
+    
+    # analyze each independent exp
+    for name in independent_exps:
+        print('[%s]' % name)
+        print_acc_for_one_exp(all_exps, name)
+        print('')
+
+if __name__ == '__main__':
+    main()
