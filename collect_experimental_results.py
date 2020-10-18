57848206dcc06c247dee1b3bd7fabfff49a8c432
Huan Wang <huanw@zju.edu.cn>
2020-10-17 22:54:36 -0400

    improve collect_experimental_results.py

--- collect_experimental_results.py
+++ collect_experimental_results.py
@@ -2,6 +2,7 @@ import sys
 import os
 import numpy as np
 import glob
+import argparse
 pjoin = os.path.join
 
 def _get_value(line, key, type_func=float, exact_key=False):
@@ -34,46 +35,71 @@ def _get_project_name():
     assert '/Projects/' in cwd
     return cwd.split('/')[-1] 
 
-def _make_acc_str(acc_list, num_digit=2):
+def _make_acc_str(acc_list, num_digit=2, present=False):
     '''Example the output: 75.84, 75.63, 75.45 -- 75.64 (0.16)
     '''
     str_format = '%.{}f'.format(num_digit)
     acc_str = [str_format % x for x in acc_list]
     mean = str_format % np.mean(acc_list) if len(acc_list) else 0
     std = str_format % np.std(acc_list) if len(acc_list) else 0
-    output = ', '.join(acc_str) + ' -- %s (%s)' % (mean, std)
+    if present:
+        output = '%s (%s)' % (mean, std) # only print mean and std
+    else:
+        output = ', '.join(acc_str) + ' -- %s (%s)' % (mean, std) # print the result of every experiment
     return output
 
-def print_acc_for_one_exp(all_exps, name, mark):
+def is_acc_line(line):
+    '''This function determines if a line is an accuracy line. Of cource the accuracy line should meet some 
+    format features which @mingsun-tse used. So if these format features are changed, this func may not work.
+    '''
+    if "Acc" in line and "Best" in line and '@' in line:
+        return True
+    else:
+        return False
+
+def parse_acc(line):
+    # last accuracy
+    if 'Acc1 =' in line: # previous impel
+        acc_l = _get_value(line, 'Acc1 =', exact_key=True)
+    else:
+        acc_l = _get_value(line, 'Acc1')
+    # best accuray
+    if 'Best Acc1' in line: # previous impel
+        acc_b = _get_value(line, 'Best Acc1', exact_key=True)
+    elif 'Best_Acc1' in line:
+        acc_b = _get_value(line, 'Best_Acc1', exact_key=True)
+    elif 'Best' in line:
+        acc_b = _get_value(line, 'Best', exact_key=True)
+    else:
+        raise NotImplementedError
+    return acc_l, acc_b
+
+def parse_epoch(line): # TODO
+    epoch = 1
+    return epoch
+
+def print_acc_for_one_exp(all_exps, name, mark, present_data):
     '''In <all_exps>, pick those with <name> in their name for accuracy collection.
     '''
     exp_id = []
-    acc_last = []
-    acc_best = []
-
+    acc_last, acc_best = [], []
     for exp in all_exps:
         if name in exp:
             log_f = '%s/log/log.txt' % exp
-            acc_l = -1
-            acc_b = -1
-            for line in open(log_f, 'r'):
-                if '%s' % mark in line: # parsing accuracy
-                    # last accuracy
-                    if 'Acc1 =' in line: # previous impel
-                        acc_l = _get_value(line, 'Acc1 =', exact_key=True)
-                    else:
-                        acc_l = _get_value(line, 'Acc1')
-
-                    # best accuray
-                    if 'Best Acc1' in line: # previous impel
-                        acc_b = _get_value(line, 'Best Acc1', exact_key=True)
-                    elif 'Best_Acc1' in line:
-                        acc_b = _get_value(line, 'Best_Acc1', exact_key=True)
-                    elif 'Best' in line:
-                        acc_b = _get_value(line, 'Best', exact_key=True)
-                    else:
-                        raise NotImplementedError
-                    break
+            acc_l, acc_b = -1, -1 # acc last, acc best
+            if mark == 'last': # the last number shown in the log
+                lines = open(log_f, 'r').readlines()
+                for k in range(1, len(lines) + 1):
+                    if is_acc_line(lines[-k]):
+                        acc_l, acc_b = parse_acc(lines[-k])
+                        epoch = parse_epoch(lines[-k]) # TODO: add epoch check
+                        break
+            
+            else: # mark is like "Epoch 240 (", which explicitly points out which epoch or step
+                for line in open(log_f, 'r'):
+                    if is_acc_line(line) and mark in line:
+                        acc_l, acc_b = parse_acc(line)
+                        break
 
             if acc_b == -1:
                 print('Not found mark "%s" in the log "%s", skip it' % (mark, log_f))
@@ -91,20 +117,23 @@ def print_acc_for_one_exp(all_exps, name, mark):
     n_digit = 2 # acc is like 75.64
     if len(acc_last) and acc_last[0] < 1: # acc is like 0.7564
         n_digit = 4
-    acc_last_str = _make_acc_str(acc_last, num_digit=n_digit)
-    acc_best_str = _make_acc_str(acc_best, num_digit=n_digit)
+    acc_last_str = _make_acc_str(acc_last, num_digit=n_digit, present='last' in present_data)
+    acc_best_str = _make_acc_str(acc_best, num_digit=n_digit, present='best' in present_data)
     print(exp_str)
     print(acc_last_str)
     print(acc_best_str)
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--kw', type=str, required=True) # to select experiment
+parser.add_argument('--mark', type=str, default='last') # 'Epoch 240' or 'Step 11200', which is used to pin down the line that prints the best accuracy
+parser.add_argument('--present_data', type=str, default='', choices=['', 'last', 'best', 'last,best'])
+args = parser.parse_args()
 def main():
     '''Usage:
         In the project dir, run:
         python ../UtilsHub/collect_experimental_results.py 20200731-18
     '''
-    kw = sys.argv[1]
-    mark = sys.argv[2] # 'Epoch 240' or 'Step 11200', which is used to pin down the line that prints the best accuracy
-    all_exps = glob.glob('Experiments/*%s*' % kw)
+    all_exps = glob.glob('Experiments/*%s*' % args.kw)
     all_exps.sort()
     
     # get independent exps, because each independent exp is run for multiple times.
@@ -117,7 +146,7 @@ def main():
     # analyze each independent exp
     for name in independent_exps:
         print('[%s]' % name)
-        print_acc_for_one_exp(all_exps, name + '_SERVER', mark)
+        print_acc_for_one_exp(all_exps, name + '_SERVER', args.mark, args.present_data)
         print('')
 
 if __name__ == '__main__':
