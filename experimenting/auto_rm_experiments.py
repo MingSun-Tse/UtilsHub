b3ace33ac781c7e453d2e1459e550d7f7cf16d84
Xingchen Zhao <xiz168@pitt.edu>
2022-10-09 17:53:17 -0400

    Update

--- experimenting/auto_rm_experiments.py
+++ experimenting/auto_rm_experiments.py
@@ -3,6 +3,8 @@ import subprocess
 import time
 import argparse
 from fnmatch import fnmatch
+sys.path.insert(0, '.')
+from smilelogging.utils import get_script_from_log
 
 #################
 r"""Usage
@@ -13,6 +15,7 @@ example: python ../UtilsHub/experimenting/auto_rm_experiments.py Experiments/
 parser = argparse.ArgumentParser()
 parser.add_argument('Experiments', type=str)
 parser.add_argument('--ignore', type=str, default=None)
+parser.add_argument('--script_wrong_mark', type=str, default=None)
 args = parser.parse_args()
 ignore = args.ignore.split(',') if args.ignore is not None else []
 
@@ -31,6 +34,12 @@ def run_shell_command(cmd, inarg=None):
     result = subprocess.run(cmd.split(), stdout=subprocess.PIPE)
     return result.stdout.decode('utf-8').strip().split('\n')
 
+
+# Rm debug and test experiments
+os.system(f'rm -rf {args.Experiments}/Debug*')
+os.system(f'rm -rf {args.Experiments}/TEST*')
+os.system(f'rm -rf {args.Experiments}/_SERVER*')
+
 # Get all exps
 exps = []
 for d in os.listdir(args.Experiments):
@@ -40,29 +49,37 @@ for d in os.listdir(args.Experiments):
         if not any(ign):
             exps += [ d_path ]
 
-ERRORS = ['RuntimeError', 'FileNotFoundError', 'AssertionError', 'TypeError', 'NameError', 'KeyError', 
-    'OSError', 'bdb.BdbQuit', 'DGLError', 'UnicodeDecodeError', '] *************************************']
 
+self_defined_errors = ['bdb.BdbQuit', '] *************************************']
 for e in exps:
     log_path = os.path.join(e, 'log', 'log.txt')
     if os.path.exists(log_path):
         last_line = run_shell_command(f'tail -n 1 {log_path}')[0]
-        # print(last_line)
-        cond = [err in last_line for err in ERRORS]
-
-        # CTRL+C
-        if last_line.startswith('KeyboardInterrupt'):
-            if len(os.listdir(os.path.join(e, 'weights'))) == 0:
-                cond += [True]
-
-        remove = False
-        if any(cond):
-            remove = True
-        if remove:
+        err = False
+        if 'Error: ' in last_line:
+            err = last_line.split(':')[0]
+            if fnmatch(err, '*Error'):
+                err = True
+        if err or any([x in last_line for x in self_defined_errors]):
             # shutil.rmtree(e)
             remove_exp(e)
-            print(f'==> Rm experiment "{e}", its last line is "{last_line}"')
+            print(f'==> Rm experiment "{e}", its last line has error: "{last_line}"')
             continue
+
+        # # CTRL+C
+        # if last_line.startswith('KeyboardInterrupt'):
+        #     if len(os.listdir(os.path.join(e, 'weights'))) == 0:
+        #         conds += [True]
+
+        # Script conditions (some experiments are run by mistake)
+        if args.script_wrong_mark is not None:
+            wrong_mark = args.script_wrong_mark.split(',')
+            script = get_script_from_log(log_path)
+            if script is not None:
+                if True in [m in script for m in wrong_mark]:
+                    remove_exp(e)
+                    print(f'==> Rm experiment "{e}", its script is wrong: "{script}"')
+                    continue
 
         # Too few lines
         lines = open(log_path).readlines()
@@ -73,7 +90,11 @@ for e in exps:
             continue
 
         # Too short time (<2min)
-        timestr = '-'.join(log_path.split('_SERVER')[1].split('/')[0].split('-')[1:])
+        try:
+            timestr = '-'.join(log_path.split('_SERVER')[1].split('/')[0].split('-')[1:])
+        except:
+            print(f'Sth wrong, log_path: {log_path}')
+            exit(0)
         start_time = time.strptime(timestr, '%Y%m%d-%H%M%S')
         start_time = time.mktime(start_time)
         last_modify_time = os.stat(log_path).st_mtime
